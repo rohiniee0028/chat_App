049dcc02567d35a17fbaa5e8e25c2986138838e0
rohini kushwaha <rohiniee0028@gmail.com>
2025-03-23 22:49:13 +0530

    implemented animation

--- src/Components/Groups.js
+++ src/Components/Groups.js
@@ -4,58 +4,63 @@ import React from 'react';
 import logo from '../Images/live-chat.png';
 import SearchIcon from '@mui/icons-material/Search';
 import { useSelector } from 'react-redux';
+import { AnimatePresence, motion } from "framer-motion"
 
 const Groups = () => {
 
-  const lightTheme = useSelector((state)=>state.themeKey)
+  const lightTheme = useSelector((state) => state.themeKey)
 
   return (
-    <div className='list-container'>
-      <div className={"ug-header" + (lightTheme ? "" : " dark")}>
-        <img src={logo} style={{ height: '2rem', width: '2rem', marginLeft: '10px' }} />
-        <p className={"ug-title" + (lightTheme ? "" : " dark")}>Available Groups</p>
-      </div>
-      <div className={'sb-search' + (lightTheme ? "" : " dark")}>
-        <IconButton className={(lightTheme ? "" : " dark")}>
-          <SearchIcon />
-        </IconButton>
-        <input placeholder='Search' className={'search-box' + (lightTheme ? "" : " dark")} />
-      </div>
-      <div className='ug-list'>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{
+          ease: "anticipate",
+          duration: "0.3"
+        }}
+
+        className='list-container'
+      >
+        <div className={"ug-header" + (lightTheme ? "" : " dark")}>
+          <img src={logo} style={{ height: '2rem', width: '2rem', marginLeft: '10px' }} />
+          <p className={"ug-title" + (lightTheme ? "" : " dark")}>Available Groups</p>
         </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
+        <div className={'sb-search' + (lightTheme ? "" : " dark")}>
+          <IconButton className={(lightTheme ? "" : " dark")}>
+            <SearchIcon />
+          </IconButton>
+          <input placeholder='Search' className={'search-box' + (lightTheme ? "" : " dark")} />
         </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
+        <div className='ug-list'>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
+          <motion.div whileHover={{ scale: 1.01 }} whileTap={{ scale: 0.98 }} className={"list-item" + (lightTheme ? "" : " dark")}>
+            <p className='con-icon'>T</p>
+            <p className={'con-title' + (lightTheme ? "" : " dark")}>Test Group</p>
+          </motion.div>
         </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
-        </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
-        </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
-        </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
-        </div>
-        <div className={"list-item" + (lightTheme ? "" : " dark")}>
-          <p className='con-icon'>T</p>
-          <p className={'con-title'+(lightTheme ? "" : " dark")}>Test Group</p>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
--- src/Components/Users.js
+++ src/Components/Users.js
@@ -3,55 +3,67 @@ import { IconButton } from '@mui/material';
 import React from 'react';
 import logo from '../Images/live-chat.png';
 import SearchIcon from '@mui/icons-material/Search';
+import { AnimatePresence, motion } from "framer-motion"
 
 const Users = () => {
   return (
-    <div className='list-container'>
-      <div className='ug-header'>
-        <img src={logo} style={{ height: '2rem', width: '2rem', marginLeft: '10px' }} />
-        <p className='ug-title'>Online Users</p>
-      </div>
-      <div className='sb-search'>
-        <IconButton>
-          <SearchIcon />
-        </IconButton>
-        <input placeholder='Search' className='search-box' />
-      </div>
-      <div className='ug-list'>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{
+          ease: "anticipate",
+          duration: "0.3"
+        }}
+        className='list-container'
+      >
+        <div className='ug-header'>
+          <img src={logo} style={{ height: '2rem', width: '2rem', marginLeft: '10px' }} />
+          <p className='ug-title'>Online Users</p>
         </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
+        <div className='sb-search'>
+          <IconButton>
+            <SearchIcon />
+          </IconButton>
+          <input placeholder='Search' className='search-box' />
         </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
+        <div className='ug-list'>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
+          <div className='list-item'>
+            <p className='con-icon'>T</p>
+            <p className='con-title'>Test User</p>
+          </div>
         </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
-        </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
-        </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
-        </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
-        </div>
-        <div className='list-item'>
-          <p className='con-icon'>T</p>
-          <p className='con-title'>Test User</p>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
